9a46e6bc47d3f26048a7765ba0d0a081e8ec0099
himanshuTiwari03 <work.himanshutw@gmail.com>
2025-07-14 14:37:37 +0530

    added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const isAuth = request.cookies.get("userCookie")?.value;
+  const currentPath = request.nextUrl.pathname;
+
+  console.log("Middleware - Path:", currentPath, "Auth:", isAuth);
+
+  const protectedRoutes = ["/cart", "/store"];
+  const isProtected = protectedRoutes.some((route) =>
+    currentPath.startsWith(route)
+  );
+
+  if (isProtected && !isAuth) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/cart/:path*", "/store/:path*"],
+};
